7f57ed6b86f5ba9d8c74ed24aa30bdf5408f7a73
Student <student@Makerss-MacBook-Pro-2.local>
2019-05-03 11:43:47 +0100

    Added the recording results feature & tests

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,13 +1,24 @@
 class Dice
+attr_reader :rolled_results
+attr_reader :score
+
+def initialize
+  @rolled_results = []
+  @score = 0
+end
+
   def roll_dice
     return (rand(6) + 1)
   end
 
   def number_of_dice(number)
-    total = 0
-    number.times {total += roll_dice}
-    total
+    score = 0
+    number.times {score += roll_dice}
+    rolled_results << score
+    return score
   end
 
-  def
+  def record_results
+    return rolled_results
+  end
 end
--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -3,14 +3,15 @@ require 'dice'
 describe Dice do
 it "dice score to be between 1 and 6" do
   expect(subject.roll_dice).to be_between(1,6).inclusive
-end
+  end
 
 it "any number of dice can be rolled" do
   number = rand(50)
   expect(subject.number_of_dice(number)).to be_between(1 * number, 6 * number).inclusive
-end
+  end
 
-it "show record of results"
-expect(record_of_results).to eq(dice.)
-
+it "show record of results" do
+  array = []
+  expect(subject.record_results).to eq(array)
+  end
 end
